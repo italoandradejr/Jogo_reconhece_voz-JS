0ec6f4a199e96faa17926a909ecf3281d9c9773e
italoandradejr <italoandradejr@gmail.com>
2023-05-07 14:26:13 -0300

    Jogo finalizado

---
+++ app/validacao.js
@@ -0,0 +1,48 @@
+function verificaSeOChutePossuiValorValido(chute) {
+    const numero = +chute
+    console.log(numero)
+
+    if (chuteForInvalido(numero)) {
+        elementoChute.innerHTML += '<div>Valor Inválido</div>'
+        return
+    }
+
+    if (numeroForMaiorOuMenorQueValorPermitido(numero)) {
+        elementoChute.innerHTML += `<div> valor inválido: valor deve estar 
+        entre ${menorValor} e ${maiorValor} </div>
+        `
+        return
+    }
+
+    if (numero === numeroSecreto) {
+        document.body.innerHTML = `
+        <h2>Você Acertou</h2>
+        <h3>O número secreto era ${numeroSecreto}</h3>
+
+        <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
+        `
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+        <div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>
+        `
+    }else {
+        elementoChute.innerHTML += `
+        <div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>
+        `
+    }
+}
+
+
+function chuteForInvalido(numero) {
+    return Number.isNaN(numero)
+}
+
+function numeroForMaiorOuMenorQueValorPermitido(numero) {
+    return numero > maiorValor || numero < menorValor
+}
+
+document.body.addEventListener('click', e => {
+    if(e.target.id == 'jogar-novamente') {
+        window.location.reload()
+    }
+})
