db582b23fa9706abe8d6df653f935eba851c15a0
italoandradejr <italoandradejr@gmail.com>
2023-05-07 15:10:22 -0300

    função fim de jogo incluida

--- app/validacao.js
+++ app/validacao.js
@@ -1,16 +1,34 @@
 function verificaSeOChutePossuiValorValido(chute) {
     const numero = +chute
+    console.log(chute)
     console.log(numero)
+    console.log(Number.isNaN(numero))
+
 
     if (chuteForInvalido(numero)) {
+        if (chute.toUpperCase() === "GAME OVER") {
+            document.body.innerHTML =
+                `
+                <h2>Game Over!!!</h2>
+                <h3>Pressione o botão para jogar novamente</h3>
+                <button id="jogar-novamente" class="btn-jogar" >Jogar novamente</button>
+                `
+                document.body.style.backgroundColor = "black";
+        } else {
+            elementoChute.innerHTML += '<div>Valor Inválido</div>';
+            return
+        }
+    }
+
+    /*if (chuteForInvalido(numero)) {
         elementoChute.innerHTML += '<div>Valor Inválido</div>'
         return
     }
+    */
 
     if (numeroForMaiorOuMenorQueValorPermitido(numero)) {
         elementoChute.innerHTML += `<div> valor inválido: valor deve estar 
-        entre ${menorValor} e ${maiorValor} </div>
-        `
+        entre ${menorValor} e ${maiorValor} </div>`
         return
     }
 
@@ -25,7 +43,7 @@ function verificaSeOChutePossuiValorValido(chute) {
         elementoChute.innerHTML += `
         <div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>
         `
-    }else {
+    } else {
         elementoChute.innerHTML += `
         <div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>
         `
@@ -37,12 +55,13 @@ function chuteForInvalido(numero) {
     return Number.isNaN(numero)
 }
 
+
 function numeroForMaiorOuMenorQueValorPermitido(numero) {
     return numero > maiorValor || numero < menorValor
 }
 
 document.body.addEventListener('click', e => {
-    if(e.target.id == 'jogar-novamente') {
+    if (e.target.id == 'jogar-novamente') {
         window.location.reload()
     }
 })
